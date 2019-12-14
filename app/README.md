3b5ef94f5b55731ee22a14cf9e6a3ee70e40b1e3
rgdl <robert_delisle44@hotmail.com>
2019-12-14 15:00:06 +1100

    added main ui elements

---
+++ app/README.md
@@ -0,0 +1,9 @@
+# Decider
+
+Decision fatigue is a real problem!
+
+In modern life, we're required to make constant, minor decisions (consider all different types of bread to choose between in the supermarket). These decisions all add up, occupying precious mental bandwidth that could otherwise be dedicated to things we actually care about.
+
+If all options are equally good, why bother making a decision at all? This is where `Decider` comes in.
+
+Just tell it how many options you're trying to decide between, hit the decide button, and it will randomly choose one for you.z
--- app/src/main/java/com/example/decider/MainActivity.java
+++ app/src/main/java/com/example/decider/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.decider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button deciderButton = (Button) findViewById(R.id.deciderButton);
+        EditText nOptions = (EditText) findViewById(R.id.nOptions);
+
+        deciderButton.setOnClickListener(
+                v -> handleClickDeciderButton(Integer.parseInt(nOptions.getText().toString()))
+        );
+
+    }
+
+    void handleClickDeciderButton(int options) {
+        Log.d("HERE!", "decided...");
     }
 }
