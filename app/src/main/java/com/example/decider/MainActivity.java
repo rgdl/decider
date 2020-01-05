55a46fa8c51ebf97a66e89b2c6612789127bff6c
rgdl <robert_delisle44@hotmail.com>
2020-01-05 14:01:56 +1100

    tidy up display, change button label

--- app/src/main/java/com/example/decider/MainActivity.java
+++ app/src/main/java/com/example/decider/MainActivity.java
@@ -11,26 +11,38 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean decisionMade = false;
+    Button deciderButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button deciderButton = (Button) findViewById(R.id.deciderButton);
+        deciderButton = (Button) findViewById(R.id.deciderButton);
+        deciderButton.setText(getString(R.string.decider_button_label_before));
         EditText nOptions = (EditText) findViewById(R.id.nOptions);
 
         deciderButton.setOnClickListener(
                 v -> handleClickDeciderButton(Integer.parseInt(nOptions.getText().toString()))
         );
-
     }
 
     void handleClickDeciderButton(int options) {
-        Random random = new Random();
-        int choice = random.nextInt(options) + 1;
+        if (!decisionMade) {
+            Random random = new Random();
+            int choice = random.nextInt(options) + 1;
 
-        TextView resultsText = (TextView) findViewById(R.id.resultsText);
+            TextView resultsText = (TextView) findViewById(R.id.resultsText);
 
-        resultsText.setText(String.format("Choose option %d", choice));
+            resultsText.setText(String.format("Choose option %d", choice));
+
+            decisionMade = true;
+            deciderButton.setText(getString(R.string.decider_button_label_after));
+        } else {
+            decisionMade = false;
+            deciderButton.setText(getString(R.string.decider_button_label_before));
+        }
+
     }
 }
