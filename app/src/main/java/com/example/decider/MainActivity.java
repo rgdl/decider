367ab95bec82e02cdcef898a72cb338edde3833c
rgdl <robert_delisle44@hotmail.com>
2020-01-05 14:33:23 +1100

    more display logic

--- app/src/main/java/com/example/decider/MainActivity.java
+++ app/src/main/java/com/example/decider/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.decider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -13,15 +13,21 @@ public class MainActivity extends AppCompatActivity {
 
     boolean decisionMade = false;
     Button deciderButton;
+    LinearLayout inputLayout;
+    LinearLayout resultsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        deciderButton = (Button) findViewById(R.id.deciderButton);
+        deciderButton = findViewById(R.id.decider_button);
         deciderButton.setText(getString(R.string.decider_button_label_before));
-        EditText nOptions = (EditText) findViewById(R.id.nOptions);
+
+        inputLayout = findViewById(R.id.input_content);
+        resultsLayout = findViewById(R.id.results_content);
+
+        EditText nOptions = findViewById(R.id.nOptions);
 
         deciderButton.setOnClickListener(
                 v -> handleClickDeciderButton(Integer.parseInt(nOptions.getText().toString()))
@@ -33,15 +39,19 @@ public class MainActivity extends AppCompatActivity {
             Random random = new Random();
             int choice = random.nextInt(options) + 1;
 
-            TextView resultsText = (TextView) findViewById(R.id.resultsText);
+            TextView resultsText = findViewById(R.id.resultsText);
 
             resultsText.setText(String.format("Choose option %d", choice));
 
             decisionMade = true;
             deciderButton.setText(getString(R.string.decider_button_label_after));
+            inputLayout.setVisibility(LinearLayout.GONE);
+            resultsLayout.setVisibility(LinearLayout.VISIBLE);
         } else {
             decisionMade = false;
             deciderButton.setText(getString(R.string.decider_button_label_before));
+            inputLayout.setVisibility(LinearLayout.VISIBLE);
+            resultsLayout.setVisibility(LinearLayout.GONE);
         }
 
     }
