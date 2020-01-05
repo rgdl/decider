b16c3695824abfd0c92fecab28f8200ce60f3dc6
rgdl <robert_delisle44@hotmail.com>
2020-01-05 16:24:07 +1100

    implement options list

--- app/src/main/java/com/example/decider/MainActivity.java
+++ app/src/main/java/com/example/decider/MainActivity.java
@@ -2,46 +2,56 @@ package com.example.decider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int INTIAL_OPTION_COUNT = 2;
 
     boolean decisionMade = false;
-    Button deciderButton;
-    LinearLayout inputLayout;
-    LinearLayout resultsLayout;
+    ArrayList<String> options = new ArrayList<>();
+
+    TextView resultsText;
+
+    private String defaultOptionText(int optionNumber) {
+        return String.format("option %d", optionNumber);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        deciderButton = findViewById(R.id.decider_button);
+        for (int i = 0; i < INTIAL_OPTION_COUNT; i++) {
+            options.add(defaultOptionText(i + 1));
+        }
+
+        ListView optionsListView = findViewById(R.id.options_list);
+        optionsListView.setAdapter(new ArrayAdapter<>(this, R.layout.options_list_item, options));
+
+        LinearLayout inputLayout = findViewById(R.id.input_content);
+        LinearLayout resultsLayout = findViewById(R.id.results_content);
+        Button deciderButton = findViewById(R.id.decider_button);
         deciderButton.setText(getString(R.string.decider_button_label_before));
+        deciderButton.setOnClickListener(v -> handleClickDeciderButton(deciderButton, inputLayout, resultsLayout));
 
-        inputLayout = findViewById(R.id.input_content);
-        resultsLayout = findViewById(R.id.results_content);
+        Button addOptionButton = findViewById(R.id.add_option_button);
+        addOptionButton.setOnClickListener(v -> handleClickAddOptionButton(optionsListView));
 
-        EditText nOptions = findViewById(R.id.nOptions);
-
-        deciderButton.setOnClickListener(
-                v -> handleClickDeciderButton(Integer.parseInt(nOptions.getText().toString()))
-        );
+        resultsText = findViewById(R.id.resultsText);
     }
 
-    void handleClickDeciderButton(int options) {
+    void handleClickDeciderButton(Button deciderButton, LinearLayout inputLayout, LinearLayout resultsLayout) {
         if (!decisionMade) {
             Random random = new Random();
-            int choice = random.nextInt(options) + 1;
-
-            TextView resultsText = findViewById(R.id.resultsText);
-
-            resultsText.setText(String.format("Choose option %d", choice));
+            String choice = options.get(random.nextInt(options.size()));
+            resultsText.setText("Choose: " + choice);
 
             decisionMade = true;
             deciderButton.setText(getString(R.string.decider_button_label_after));
@@ -54,5 +64,10 @@ public class MainActivity extends AppCompatActivity {
             resultsLayout.setVisibility(LinearLayout.GONE);
         }
 
+    }
+
+    void handleClickAddOptionButton(ListView optionsListView) {
+        options.add(defaultOptionText(options.size() + 1));
+        optionsListView.invalidateViews();
     }
 }
